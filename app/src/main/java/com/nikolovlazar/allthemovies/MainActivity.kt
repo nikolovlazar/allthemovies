d75fc86d87970e8113d77d1e26d3b04c5e898ed7
Lazar Nikolov <lazar.nikolov@sentry.io>
2023-01-26 16:46:27 +0100

    moved a method in utils

--- app/src/main/java/com/nikolovlazar/allthemovies/MainActivity.kt
+++ app/src/main/java/com/nikolovlazar/allthemovies/MainActivity.kt
@@ -53,16 +53,3 @@ class MainActivity : ComponentActivity() {
     }
   }
 }
-
-fun getJsonDataFromAsset(context: Context, fileName: String): String? {
-  val jsonString: String
-  try {
-    jsonString = context.assets.open(fileName).bufferedReader().use {
-      it.readText()
-    }
-  } catch (ioException: IOException) {
-    ioException.printStackTrace()
-    return null
-  }
-  return jsonString
-}
--- app/src/main/java/com/nikolovlazar/allthemovies/utils/fetchMovies.kt
+++ app/src/main/java/com/nikolovlazar/allthemovies/utils/fetchMovies.kt
@@ -4,7 +4,20 @@ import android.content.Context
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.nikolovlazar.allthemovies.Movie
-import com.nikolovlazar.allthemovies.getJsonDataFromAsset
+import java.io.IOException
+
+fun getJsonDataFromAsset(context: Context, fileName: String): String? {
+  val jsonString: String
+  try {
+    jsonString = context.assets.open(fileName).bufferedReader().use {
+      it.readText()
+    }
+  } catch (ioException: IOException) {
+    ioException.printStackTrace()
+    return null
+  }
+  return jsonString
+}
 
 fun fetchMovies(context: Context): List<Movie> {
   val jsonFileString = getJsonDataFromAsset(context, "movies.json")
